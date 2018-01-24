851e994b96d7a62c1fa905e59435f99a912418a4
vpkirby002 <vpkirby002@gmail.com>
2018-01-24 14:56:36 -0500

    first commit

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react'; 
+
+export default class Home extends Component {
+	static propTypes = {};
+
+render () {
+	return (
+		<div>
+		<h1>Hey Val! You're a real life Web Developer, sis, and your React app is live! You're literally amazing. </h1>
+		</div>
+		);
+	}
+}
