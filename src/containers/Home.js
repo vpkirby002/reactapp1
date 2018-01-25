4a9776f10f6120b4ba9be77609fbf50c6a7cacde
vpkirby002 <vpkirby002@gmail.com>
2018-01-24 19:58:47 -0500

    added hand on laptop image

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,11 @@
 import React, { Component, PropTypes } from 'react'; 
+import styled from 'styled-components';
+
+const Image = styled.img`
+	width: 100%;
+		
+		`;
+
 
 export default class Home extends Component {
 	static propTypes = {};
@@ -7,6 +14,7 @@ render () {
 	return (
 		<div>
 		<h1>Hey Val! You're a real life Web Developer, sis, and your React app is live! You're literally amazing. </h1>
+		<Image src={require('../assets/hand_on_laptop_copy.jpg')} alt="hand-image"/>
 		</div>
 		);
 	}
